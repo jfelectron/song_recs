4b7358d2c612c849529aec314b01c5def1c1d644
Jonathan Foley <jonefoley@gmail.com>
2016-01-17 16:09:10 -0800

    fix bugs

--- song_reccomender.py
+++ song_reccomender.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from operator import itemgetter
+import pdb
 
 import pandas as pd
 import numpy as np
@@ -25,13 +26,12 @@ class SongRecommender(object):
         self.user_profiles = None
         self.song_names = []
         self.song_features = []
-
-
+        self._prep_data()
 
     def transform_data(self):
-
-        self.song_vectors = self._song_tfidf()
+        self.song_vectors = self._song_tfidf().todense()
         self.user_profiles = self._user_tastes()
+        print("transformed song and user preference data")
 
     def recommend(self, user, n_songs=3):
         """
@@ -40,15 +40,15 @@ class SongRecommender(object):
         :param n_songs:
         :return:
         """
-
-        predicted_tastes = self.user_profiles[user].multiply(self.song_vectors)
+        predicted_tastes = np.squeeze(np.asarray(self.song_vectors.dot(self.user_profiles.loc[user].values)))
         # get mask of already listened
-        already_listend = self.data["user"].iloc[user, self.data["user"] != 0]
-        self.masked_preds = predicted_tastes.iloc[already_listend] = 0
-        zip(song_names,self.masked_preds)
-        return self.song_names[]
-
-
+        already_listend = self.data["user"][user].loc[self.data["user"][user] != 0]
+        listened_index = [self.song_names.index(song) for song in already_listend.index]
+        np.put(predicted_tastes,listened_index,-10)
+        name_preds = zip(self.song_names, predicted_tastes)
+        s_scores = [s_score for s_score in name_preds]
+        s_scores.sort(key=itemgetter(1),reverse=True)
+        return s_scores[:n_songs]
 
     def _prep_data(self):
         """
@@ -65,20 +65,23 @@ class SongRecommender(object):
         """
 
         user_prefs = self.data["user"]
+        user_profiles = np.zeros((len(user_prefs.columns), len(self.data["song"].columns)))
 
-        for i, user in enumerate(user_prefs.columns()):
-            user_profiles = np.zeros(len(user_prefs), len(self.data["song"].columns))
-            user_profiles[i, :] = self.song_vectors.transpose().multiply(user_prefs[user], fill_value=0)
-            self.user_profiles = pd.DataFrame(data=user_profiles, index=self.data["user"].columns,
-                                              columns=self.song_features)
+        for i, user in enumerate(user_prefs.columns):
+            user_profiles[i, :] = self.song_vectors.transpose().dot(user_prefs[user])
+
+        prefs_df= pd.DataFrame(data=user_profiles, index=self.data["user"].columns, columns=self.song_features)
+        return prefs_df
+
 
     def _song_tfidf(self):
         """
 
         :return:
         """
-        self.songs = list(self.data["song"].index)
+        self.song_names = list(self.data["song"].index)
         self.song_features = list(self.data["song"].columns)
+        self.tfidf.fit(self.data["song"])
         return self.tfidf.transform(self.data["song"])
 
     def _check_data_path(self):
@@ -87,7 +90,7 @@ class SongRecommender(object):
         """
         if not self.path:
             dirname, fname = os.path.split(os.path.abspath(__file__))
-            self.path = '/'.join(dirname.split('/')[:-1]) + "/data/"
+            self.path = dirname + "/data"
 
         print("using data path: {0}".format(self.path))
 
@@ -97,12 +100,11 @@ class SongRecommender(object):
 
         """
         try:
-            # strip trailing / if exists
-            path = self.path[:-1] if self.path[-1] == "/" else self.path
-            data_files = glob.glob("{0}/*.csv".format(path))
+            path = self.path
+            data_files = glob.glob("{0}/*.csv".format(self.path))
             for csv in data_files:
                 # with current files results in engagement and users
-                data_key = csv.split("_")[0]
+                data_key = csv.split(".csv")[0].split("/")[-1].split("_")[0]
                 self.data[data_key] = pd.DataFrame.from_csv(csv).fillna(0)
                 print("loaded %s" % csv)
 
